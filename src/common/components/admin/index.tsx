4c9b05f9b5549364f9b2c3c67fb221655c2f9711
Dhawan Kumar <r.dhawan27@hotmail.co.uk>
2020-04-07 17:41:48 +0000

    fix: Container added for left and right margin

--- src/common/components/admin/index.tsx
+++ src/common/components/admin/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+
 import Admin from './Admin';
 import AddAdmin from './AddAdmin';
 
 export default function AdminHomePage() {
-  return <Admin />;
+  return (
+    <Container>
+      <Admin />
+    </Container>
+  );
 }
 export { AddAdmin };
