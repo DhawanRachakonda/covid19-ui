b8423b37ba010b51c6ecdaddbd0184d4355a33b3
explorervarun <varunmyadam@gmail.com>
2020-04-07 16:13:03 +0530

    legend added and swi for admin

--- src/common/components/home/index.tsx
+++ src/common/components/home/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-
+import {Card} from 'react-bootstrap';
 import Map from './Map';
+import { DivIcon } from 'leaflet';
 import { UploadVisitedPlacesContextProder } from '../providers/UploadUserVisitedPlacesProvider';
 
 export default () => {
   return (
     <UploadVisitedPlacesContextProder>
       <Map />
+      <div> 
+        <Card className="text-center">
+        <p>
+          <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" fill="red" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="8" cy="8" r="8"/>
+          </svg>
+           - Infected areas on selected date
+        </p>
+        
+          </Card>
+      </div>
     </UploadVisitedPlacesContextProder>
   );
 };
